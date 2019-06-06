68d670d79925023f80faaee15be639983a478f6c
frederik <frederik.degrove@ugent.be>
2019-06-06 09:16:46 +0200

    update

--- data exploration.py
+++ data exploration.py
@@ -2,16 +2,96 @@ import pandas as pd
 import numpy as np
 import csv
 import matplotlib.pyplot as plt
+import json
+import codecs
+import unicodedata
 
 #starting up jupyter notebook
 #jupyter notebook
 
-#location = "/home/frederik/Documents/Data GOA/hln_comments.csv"
 
-#data = pd.read_csv(location, sep=",",quotechar='\"', header=None, doublequote=False, nrows=62, escapechar="\\")
-location = "comment_data.csv"
-#data = pd.read_csv(location, sep=",",quotechar='\"', header=None, doublequote=False, nrows=62, escapechar="\\")
+#### full data
 
+location = "hln_articles.csv"
+#utf-8-sig
+dat2 = []
+with codecs.open(location, 'r', encoding="utf-8") as csv_file:
+    csv_reader = csv.reader(csv_file, delimiter='\t')
+    for row in csv_reader:
+        if len(row) == 10:
+            dat2.append(row)
+        else:
+            print("NOK")
+
+cols = dat2[0]
+dat2.pop(0)
+
+
+title = []
+text = []
+for index, i in enumerate(dat2):
+    if index != 0:
+        da = i[9].split('}')
+        if len(da) >= 2:
+            title.append(da[0].replace("\\", "").replace('\"', ''))
+            text.append(i[9][len(da[0]):])
+        else:
+            print(index)
+
+
+
+len(title) == len(dat2)
+
+for index, i in enumerate(dat2):
+    if index != 0:
+        da = i[9].split('}')
+        if len(da) > 3:
+            print(index)
+
+
+def replaceMultiple(mainString, toBeReplaces, newString):
+    # Iterate over the strings to be replaced
+    for elem in toBeReplaces:
+        # Check if string is in the main string
+        if elem in mainString:
+            # Replace the string
+            mainString = mainString.replace(elem, newString)
+    return mainString
+
+to_replace_title = ["rawText:", "\\", "HEADER", "[", "]", "{", "}", "textType"]
+title2 = [replaceMultiple(i, to_replace_title, "") for i in title]
+
+to_replace_text = ['rawText', "\\", "HEADER", "[", "]", "{", "}", "textType", "PARAGRAPH", "SUBTITLE", "INTRO", ":", '\"', 'u0027', 'u0026']
+text2 = [replaceMultiple(i, to_replace_text, "") for i in text]
+text2 = [i.replace(u'\xa0', u' ') for i in text2]
+text2 = [i.replace(',', ' ') for i in text2]
+text2 = [i.replace("  ", " ") for i in text2]
+
+p = pd.DataFrame(dat2)
+p.columns = cols
+o = p.text
+t = [i.replace('\\\\\\\"', ' ') for i in o]
+t2 = [i.replace('\\', '') for i in t]
+
+
+k = list(unicodedata.normalize('NFKD',t2[1]))
+k.insert(2, '"')
+
+m = ''.join(k)
+n = m[0:len(m)-1]
+f = json.loads(n)
+print(pd.DataFrame(f))
+
+
+s = p.groupby('section')
+s.shares.describe()
+s.shortId.describe()
+
+s.shortId.describe()['count']
+
+
+#old
+'''
 
 
 
@@ -76,59 +156,4 @@ new = grouptitle['comment'].count()
 
 p = pd.DataFrame(new)
 p['title'] = new.index
-
-
-
-#### full data
-
-location = "hln_articles.csv"
-
-dat2 = []
-with open(location, encoding="utf-8-sig") as csv_file:
-    csv_reader = csv.reader(csv_file, delimiter='\t')
-    for row in csv_reader:
-        if len(row) == 10:
-            dat2.append(row)
-        else:
-            print("NOK")
-
-title = []
-text = []
-for index, i in enumerate(dat2):
-    if index != 0:
-        da = i[9].split('}')
-        if len(da) >= 2:
-            title.append(da[0].replace("\\", "").replace('\"', ''))
-            text.append(i[9][len(da[0]):])
-        else:
-            print(index)
-
-cols = dat2[0]
-dat2.pop(0)
-
-len(title) == len(dat2)
-
-for index, i in enumerate(dat2):
-    if index != 0:
-        da = i[9].split('}')
-        if len(da) > 3:
-            print(index)
-
-
-def replaceMultiple(mainString, toBeReplaces, newString):
-    # Iterate over the strings to be replaced
-    for elem in toBeReplaces:
-        # Check if string is in the main string
-        if elem in mainString:
-            # Replace the string
-            mainString = mainString.replace(elem, newString)
-    return mainString
-
-to_replace_title = ["rawText:", "\\", "HEADER", "[", "]", "{", "}", "textType"]
-title2 = [replaceMultiple(i, to_replace_title, "") for i in title]
-
-to_replace_text = ['rawText', "\\", "HEADER", "[", "]", "{", "}", "textType", "PARAGRAPH", "SUBTITLE", "INTRO", ":", '\"', 'u0027', 'u0026']
-text2 = [replaceMultiple(i, to_replace_text, "") for i in text]
-text2 = [i.replace(u'\xa0', u' ') for i in text2]
-text2 = [i.replace(',', ' ') for i in text2]
-text2 = [i.replace("  ", " ") for i in text2]
+'''
