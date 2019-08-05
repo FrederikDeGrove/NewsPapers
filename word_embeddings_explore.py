3264d3a0789e3190c6df239478ea262331cd89aa
frederik <frederik.degrove@ugent.be>
2019-08-05 12:12:00 +0200

    update

--- word_embeddings_explore.py
+++ word_embeddings_explore.py
@@ -1,3 +1,6 @@
+from sklearn.model_selection import train_test_split
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
 from keras.models import load_model
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -47,10 +50,11 @@ row2.insert(0, "empty")
 index_to_words = dict(zip(row2, indices))
 
 model = load_model("feedforward_embed_hidden101.hdf5")
-
+model.summary()
 embeddings = model.get_weights()[0]
 words_embeddings = {w : embeddings[idx] for w, idx in index_to_words.items()}
 embeds = pd.DataFrame.from_dict(words_embeddings, orient='index')
+embeds.shape
 
 ###########################################################
 ###########################################################
@@ -75,7 +79,7 @@ ax2.spines['left'].set_visible(False)
 plt.hist(embeds[1], bins=80 ,  fc=(0, 0.3, 0.3, 0.3))
 
 
-# looking at some of the words that have scores that diverge from 0
+# looking at some of the words that have scores that are larger or smaller than a certain threshold
 
 threshold = .1
 high = embeds[(embeds[0] > threshold) | (embeds[1] > threshold)]
@@ -102,48 +106,46 @@ max_embeds_D1 = full.sort_values(by=['dim1'], ascending=False)
 max_embeds_D2 = full.sort_values(by=['dim2'], ascending=False)
 
 # 10 highest scores
-max_embeds_D1.iloc[1:10]
-max_embeds_D2.iloc[1:10]
+print(max_embeds_D1.iloc[1:10])
+print(max_embeds_D2.iloc[1:10])
 
 # 10 lowest scores
-max_embeds_D1.iloc[-10:]
-max_embeds_D2.iloc[-10:]
+print(max_embeds_D1.iloc[-10:])
+print(max_embeds_D2.iloc[-10:])
 
 
 # having a look at how high to low scoring words behave when used in the model
 # to do so, first initialize the tokenizer from the neural_networks file
 '''
-text = X_train.title
-text = text.values.tolist()
+dat = pd.read_csv("raw_HLN.csv", index_col = None)
+dat.title = dat.title.astype("str")
+X_train, X_test, y_train, y_test = train_test_split(dat.title, dat.views, test_size=0.15, random_state=123)
+text = X_train.values.tolist()
 tokenizer = Tokenizer(filters='', lower=True)
 tokenizer.fit_on_texts(texts=text)
 '''
 
+# predict for negative embeddings scores
 predict_for_sentence(max_embeds_D1.iloc[-15:].index)
 predict_for_sentence(max_embeds_D2.iloc[-15:].index)
+# predict for positive embeddings scores
 predict_for_sentence(max_embeds_D1.iloc[:15].index)
 predict_for_sentence(max_embeds_D2.iloc[:15].index)
-
-# get words with top and bottom embedding scores for both dimensions
-max_embeds_D1.iloc[-10:]
-max_embeds_D2.iloc[-10:]
-max_embeds_D1.iloc[:10]
-max_embeds_D2.iloc[:10]
 
 
 # what about scores close to zero on embeddings
 threshold = .000001
 zeros = embeds[(embeds[0] > -threshold) & (embeds[0] < threshold) & (embeds[1] > -threshold) & (embeds[1] < threshold) ]
 # each word gives a slight increase in prediction - not linear however
-predict_for_sentence(zeros.iloc[:16].index)
+predict_for_sentence(zeros.iloc[:1].index)
 predict_for_sentence(zeros.iloc[:6].index)
+predict_for_sentence(zeros.iloc[:16].index)
 
 # what about slightly positive values on embeddings
 threshold1 = .02
 threshold2 = .03
-
 small = embeds[(embeds[0] > threshold1) & (embeds[0] < threshold2) & (embeds[1] > threshold1) & (embeds[1] < threshold2) ]
-# even very close to zero word embeddings give a prediction > .50 when enough of them are combined
+# word embeddings give a prediction > .50 when enough of them are combined
 predict_for_sentence(small.iloc[:1].index)
 predict_for_sentence(small.iloc[:16].index)
 
@@ -157,12 +159,14 @@ preds = pd.DataFrame.from_dict(predictions, orient='index')
 preds.columns= ['prediction']
 preds.sort_values(by='prediction', ascending=False)
 # preds.to_csv("word_predictions_embeddings.csv")
+# predictions = pd.read_csv("word_predictions_embeddings.csv")
 
-# have a look what happens when predicting with the top word
+
+# have a look what happens when predicting with the top word once and twice
 predict_for_sentence(['overlijdt'])
 predict_for_sentence(['overlijdt', 'overlijdt'])
 
-# having a look at the weight and bias parameters
+# let's have a look at the weight and bias parameters
 W_Layer_1 = model.get_weights()[1]
 B_Layer_1 = model.get_weights()[2]
 W_Layer_2 = model.get_weights()[3]
@@ -171,3 +175,4 @@ B_Layer_2 = model.get_weights()[4]
 pd.DataFrame(W_Layer_1).describe()
 pd.DataFrame(B_Layer_1).describe()
 pd.DataFrame(W_Layer_2).describe()
+B_Layer_2
